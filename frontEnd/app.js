55a9f21d0759b539f142f9021258719b663ea08d
kbhanush <kris.bhanushali@oracle.com>
2014-12-14 14:08:23 -0800

    fixed WS disconnects. Good working condition with sustained connection. Mongo clock still sticks. Next -  jazz it up with bootstrap theme  make work with oracle 12c

--- frontEnd/app.js
+++ frontEnd/app.js
@@ -1,31 +1,63 @@
 
 var app = angular.module('app', ['ngRoute']);
 
+
+
+
+
 //--------------Websocket connection initiator ------------------
 angular.module('app')
     .run(function ($rootScope,$timeout,  $window) {
-        var host
-        if ($window.location.protocol === "https:") {
-            host = "wss://" + $window.location.host
-        } else {
-            host = "ws://" + $window.location.host
-        }
-        var connection = new WebSocket(host)
 
-        connection.onopen = function () {
-            console.log('WebSocket connected')
-        }
+//        var connect = function(){
+//
+//            var host = "ws://changu.ngrok.com"
+//
+//            var connection = new WebSocket(host)
+//
+//            connection.onopen = function () {
+//                console.log('WebSocket connected')
+//
+//                connection.onclose(function() {
+//                    console.log('Connection dropped, reconnecting...')
+//                    connect()
+//                    connection.onmessage = function (e) {
+//                        var payload = JSON.parse(e.data)
+//                        $rootScope.$broadcast('ws:' + payload.topic, payload.data)
+//                    }
+//                })
+//            }
+//        }
 
-        connection.onclose = function (e) {
-            console.log('WebSocket closed. Reconnecting...')
-            $timeout(connect, 10*1000)
-        }
+    var connect = function(){
+            var host1
+            if ($window.location.protocol === "https:") {
+                host = "wss://" + $window.location.host
+            } else {
+                host = "ws://" + $window.location.host
+            }
+            var connection = new WebSocket(host)
 
-        connection.onmessage = function (e) {
-            var payload = JSON.parse(e.data)
-            $rootScope.$broadcast('ws:' + payload.topic, payload.data)
-        }
-    })
+            connection.onopen = function () {
+                console.log('WebSocket connected')
+            }
+
+            connection.onclose = function (e) {
+                console.log('WebSocket closed. Reconnecting...')
+
+                connect();
+
+                console.log('websocket reconnected')
+            }
+
+            connection.onmessage = function (e) {
+                var payload = JSON.parse(e.data)
+                $rootScope.$broadcast('ws:' + payload.topic, payload.data)
+            }
+    }
+
+        connect();
+  })
 
 //------------------------------------------------------------------------
 angular.module('app')
